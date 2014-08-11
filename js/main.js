5ddd9f2c5c1def3db697859f273f34467eec2c6f
shashank sharma <shashank9487@gmail.com>
2014-08-11 14:44:41 +0530

    add empployee detail section with new modular employee structured class

--- js/main.js
+++ js/main.js
@@ -8,10 +8,52 @@ var app = {
         }
     },
 
+    registerEvents: function() {
+        $(window).on('hashchange', $.proxy(this.route, this));
+        var self = this;
+        /*check if browser support touch*/
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            /*if yes, change register touch event*/
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // if not supported: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
+
+    route: function() { 
+        var hash = window.location.hash;
+        if(!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        //console.log(match);
+        if(match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                //console.log(employee);
+                $('body').html(new EmployeeView(employee).render().el);
+                console.log($('body').html(new EmployeeView(employee).render().el));
+            });
+        }
+    },
+
     initialize: function() {
         var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
+            self.route();
         });
     }
 
