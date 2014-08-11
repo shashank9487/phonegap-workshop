7806f6fe1a39fd2657c0d2ce59320dff79c9b6d2
shashank sharma <shashank9487@gmail.com>
2014-08-11 15:41:46 +0530

    add contact api

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -3,7 +3,29 @@ var EmployeeView = function(employee) {
 	this.initialize = function() {
 		this.el = $('<div/>');
 		this.el.on('click', '.add-location-btn', this.addLocation);
+		this.el.on('click', '.add-contact-btn', this.addToContacts);
 	};
+
+	this.addToContacts = function(event) {
+		event.preventDefault();
+		console.log('addToContacts');
+		if (!navigator.contacts) {
+			app.showAlert('Contacts Api not supported', 'Error');
+			return;
+		};
+		var contact = navigator.contacts.create();
+		contact.name = {
+			givenName: employee.firstName,
+			familyName: employee.lastName
+		}
+		var phoneNumbers = [];
+		phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+		phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, false);
+		contact.phoneNumbers = phoneNumbers;
+		contact.save();
+		console.log(contact);
+		return false; 
+	}
 
 	this.addLocation = function(event) {
 		event.preventDefault();
