c73c6ffb646005319e33ffc7bbecc2907f261d37
shashank sharma <shashank9487@gmail.com>
2014-08-11 13:11:13 +0530

    add new class, structured code

--- js/main.js
+++ js/main.js
@@ -1,31 +1,18 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     },
 
     initialize: function() {
         var self = this;
-        this.store = new MemoryStore(function(){
-            self.showAlert('Store initialized', 'Info');
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-    showAlert: function(message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title? (title + ": " + message) : message);
-        }
     }
 
 };
