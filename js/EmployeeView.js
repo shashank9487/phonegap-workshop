8c04dd7ff864b48717ffdae7bbfe6de6cf95f613
shashank sharma <shashank9487@gmail.com>
2014-08-11 15:31:03 +0530

    add location api

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -2,6 +2,21 @@ var EmployeeView = function(employee) {
 
 	this.initialize = function() {
 		this.el = $('<div/>');
+		this.el.on('click', '.add-location-btn', this.addLocation);
+	};
+
+	this.addLocation = function(event) {
+		event.preventDefault();
+		console.log('addLocation');
+		navigator.geolocation.getCurrentPosition(
+			function(position) {
+				console.log(position);
+				$('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+			},
+			function() {
+				alert('Error getting location');
+			});
+		return false;
 	};
 
 	this.render = function() {
